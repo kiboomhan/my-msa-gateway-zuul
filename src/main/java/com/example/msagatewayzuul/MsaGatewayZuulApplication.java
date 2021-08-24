0078c398abe533778850798de2702ef07fbe36a9
kiboomhan <kiboomhan@gmail.com>
2021-08-24 20:52:48 +0900

    프로젝트 설정

---
+++ src/main/java/com/example/msagatewayzuul/MsaGatewayZuulApplication.java
@@ -0,0 +1,17 @@
+package com.example.msagatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class MsaGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaGatewayZuulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/msagatewayzuul/MsaGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msagatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
